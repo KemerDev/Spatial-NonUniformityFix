68897d92082f1d3759b72ae8b31a3ff28131ed09
kemerios <kemerios15@hotmail.gr>
2022-10-20 21:20:17 +0200

    test

--- EmguCvInter/CropAndNormal.cs
+++ EmguCvInter/CropAndNormal.cs
@@ -10,13 +10,9 @@ using Emgu.CV.Util;
 using Emgu.CV.Structure;
 using Emgu.CV.CvEnum;
 using Emgu.CV.UI;
-using Numpy;
 using System.Diagnostics;
 using System.Drawing.Imaging;
-using static System.Net.Mime.MediaTypeNames;
-using System.Windows.Forms.VisualStyles;
 using Emgu.CV.Reg;
-using System.Security.Cryptography;
 
 namespace EmguCvInter
 {
@@ -29,7 +25,7 @@ namespace EmguCvInter
         private int Count;
 
         public CropAndNormal(PictureBox p2, PictureBox p3, PictureBox p4, int Count)
-        { 
+        {
             this.p2 = p2;
             this.p3 = p3;
             this.p4 = p4;
@@ -38,6 +34,12 @@ namespace EmguCvInter
 
         public void CropImage(Image<Bgr, Byte> unprocessedImage, Image<Gray, Byte> nivImage)
         {
+            SkewCorrection newSkew = new SkewCorrection();
+
+            double[] matMax;
+            double[] matMin;
+            Point[] matMinLoc;
+            Point[] matMaxLoc;
 
             float[,] dilateKernel =
             {
@@ -46,80 +48,24 @@ namespace EmguCvInter
                 { 1, 1, 1}
             };
 
-            double[] matMax;
-            double[] matMin;
-            Point[] matMinLoc;
-            Point[] matMaxLoc;
-
-            ConvolutionKernelF dilateMatrixKernel = new ConvolutionKernelF(dilateKernel);;
+            ConvolutionKernelF dilateMatrixKernel = new ConvolutionKernelF(dilateKernel);
 
             try
             {
                 // 35ms run time
-                var original = unprocessedImage.Copy().Rotate(4, new Bgr());
+                var original = newSkew.SkewOrg(nivImage);
 
-                var unProsImg = unprocessedImage.Convert<Gray, byte>().Rotate(4, new Gray());
+                var skewNiv = newSkew.SkewNiv(nivImage);
+
+                this.p2.Image = original;
 
                 /*Stopwatch sw = Stopwatch.StartNew();
                 sw.Stop();
                 Debug.WriteLine("Gray bitlock Time taken: {0}ms", sw.Elapsed.TotalMilliseconds);*/
-                
+
                 // --------------
 
-                var x1Point = new Point();
-                var x2Point = new Point();
-
-                var corners = new Image<Gray, float>(original.Size);
-
-                // 69ms run time
-                CvInvoke.CornerHarris(unProsImg, corners, 10, 19, 0.04);
-                // --------------
-
-                CvInvoke.Dilate(corners, corners, dilateMatrixKernel, new Point(0, 0), 2, 0, new MCvScalar(255, 0, 0));
-
-                corners.MinMax(out matMin, out matMax, out matMinLoc, out matMaxLoc);
-
-                // 0.0045ms run time
-                for (int i = 530; i < 540; i++)
-                {
-                    for (int j = 255; j < 280; j++)
-                    {
-                        if (corners.Data[j, i, 0] > 0.003 * matMax[0])
-                        {
-                            x1Point.X = i;
-                            x1Point.Y = j;
-                        }
-                    }
-                }
-
-                for (int i = 1970; i < 1985; i++)
-                {
-                    for (int j = 1725; j < 1755; j++)
-                    {
-                        if (corners.Data[j, i, 0] > 0.003 * matMax[0])
-                        {
-                            x2Point.X = i;
-                            x2Point.Y = j;
-                        }
-                    }
-                }
-                // --------------
-
-                var rect = createRectangle(x1Point.X, x1Point.Y, x2Point.X, x2Point.Y);
-
-                unProsImg.ROI = rect;
-                nivImage.ROI = rect;
-                var cropedTile = unProsImg.Copy();
-                var cropedNiv = nivImage.Copy();
-
-                original.ROI = Rectangle.Empty;
-                nivImage.ROI = Rectangle.Empty;
-
-
-                this.p2.Image = cropedTile.ToBitmap();
-                this.p3.Image = cropedNiv.ToBitmap();
-
-                Nivalation(cropedTile.ToBitmap(), cropedNiv.ToBitmap());
+                //Nivalation(unprocessedImage.ToBitmap(), nivImage.ToBitmap());
                 // --------------
 
                 //((col > 200 && col < 280 && row > 500 && row < 550) || 
@@ -130,7 +76,11 @@ namespace EmguCvInter
             }
             catch (Exception ex)
             {
-                MessageBox.Show(ex.Message);
+                MessageBox.Show(ex.ToString());
+            }
+            finally
+            {
+                newSkew.Dispose();
             }
         }
 
@@ -139,57 +89,74 @@ namespace EmguCvInter
 
             var nivalated = new Bitmap(cropedNiv.Width, cropedNiv.Height, PixelFormat.Format32bppRgb);
             string savePath = "C:\\Users\\kemerios\\Desktop\\imageProcess\\procesed\\";
-            int pixelData = 0;
+            double[,] pixelData = new double[nivalated.Width, nivalated.Height];
 
             try
             {
-                Stopwatch sw = Stopwatch.StartNew();
-
                 unsafe
                 {
                     BitmapData bitmapDataTile = cropedTile.LockBits(new Rectangle(0, 0, cropedTile.Width, cropedTile.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppRgb);
                     BitmapData bitmapDataNiv = cropedNiv.LockBits(new Rectangle(0, 0, cropedNiv.Width, cropedNiv.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppRgb);
-                    BitmapData bitmapDataNivalated = nivalated.LockBits(new Rectangle(0, 0, nivalated.Width, nivalated.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppRgb);
 
                     byte* TilePointer = (byte*)bitmapDataTile.Scan0;
                     byte* NivPointer = (byte*)bitmapDataNiv.Scan0;
-                    byte* NivalatedPointer = (byte*)bitmapDataNivalated.Scan0;
+
+                    Stopwatch sw = Stopwatch.StartNew();
 
                     for (int i = 0; i < bitmapDataTile.Height; i++)
                     {
                         for (int j = 0; j < bitmapDataTile.Width; j++)
                         {
-                            if (TilePointer[0] == 0 && NivPointer[0] > 0)
-                            {
-                                pixelData = TilePointer[0] / NivPointer[0];
-                            }
-                            else
-                            {
-                                pixelData = NivPointer[0] / TilePointer[0];
-                            }
-
-                            NivalatedPointer[0] = (byte)pixelData;
-                            NivalatedPointer[1] = (byte)pixelData;
-                            NivalatedPointer[2] = (byte)pixelData;
+                            pixelData[j, i] = Math.Sqrt(255 * TilePointer[0] / (NivPointer[0] + 1));
 
                             TilePointer += 4;
                             NivPointer += 4;
-                            NivalatedPointer += 4;
                         }
 
                         TilePointer += bitmapDataTile.Stride - (bitmapDataTile.Width * 4);
                         NivPointer += bitmapDataNiv.Stride - (bitmapDataNiv.Width * 4);
-                        NivalatedPointer += bitmapDataTile.Stride - (bitmapDataTile.Width * 4);
                     }
 
-                    nivalated.UnlockBits(bitmapDataNivalated);
+                    sw.Stop();
+                    Debug.WriteLine("Time taken: {0}ms", sw.Elapsed.TotalMilliseconds);
+
                     cropedTile.UnlockBits(bitmapDataTile);
                     cropedNiv.UnlockBits(bitmapDataNiv);
 
                 }
 
-                sw.Stop();
-                Debug.WriteLine("Time taken: {0}ms", sw.Elapsed.TotalMilliseconds);
+                double maxPixelVal = 0;
+
+                for (int i = 0; i < pixelData.GetLength(1); i++)
+                {
+                    for (int j = 0; j < pixelData.GetLength(0); j++)
+                    {
+                        maxPixelVal = Math.Max(maxPixelVal, pixelData[j, i]);
+                    }
+                }
+
+                unsafe
+                {
+                    BitmapData bitmapDataNiv = nivalated.LockBits(new Rectangle(0, 0, nivalated.Width, nivalated.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppRgb);
+                    byte* NivalatedPointer = (byte*)bitmapDataNiv.Scan0;
+
+                    for (int i = 0; i < nivalated.Height; i++)
+                    {
+                        for (int j = 0; j < nivalated.Width; j++)
+                        {
+
+                            var temp = Convert.ToInt32((pixelData[j, i] * 255) / maxPixelVal);
+
+                            NivalatedPointer[0] = (byte)temp;
+                            NivalatedPointer[1] = (byte)temp;
+                            NivalatedPointer[2] = (byte)temp;
+
+                            NivalatedPointer += 4;
+                        }
+                        NivalatedPointer += bitmapDataNiv.Stride - (bitmapDataNiv.Width * 4);
+                    }
+                    nivalated.UnlockBits(bitmapDataNiv);
+                }
 
                 nivalated.Save(savePath + Count.ToString() + ".png", ImageFormat.Png);
             }
---
+++ EmguCvInter/SkewCorrection.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Util;
+using Emgu.CV.Structure;
+using Emgu.CV.CvEnum;
+using Emgu.CV.UI;
+using System.Diagnostics;
+using System.Drawing.Imaging;
+using Emgu.CV.Reg;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace EmguCvInter
+{
+    public class SkewCorrection : IDisposable
+    {
+        public bool Disposed;
+        Point anglePoint1 = new Point();
+        Point anglePoint2 = new Point();
+
+        public Bitmap SkewOrg(Image<Gray, Byte> img)
+        {
+            var bmp = img.ToBitmap();
+
+            var img_height = bmp.Height;
+            var img_width = bmp.Width;
+
+            int x = img_width / 2;
+            int y = img_height / 2;
+
+            Color idxColor = bmp.GetPixel(x, y);
+
+            Debug.WriteLine(idxColor);
+
+            Color colorT = Color.FromArgb(255, 255, 255, 255);
+            Color colorF = Color.FromArgb(255, 1, 1, 1);
+
+            return FloodFill(bmp, idxColor, colorT, colorF); ;
+        }
+
+        private Bitmap FloodFill(Bitmap bmp, Color idxColor, Color colorT, Color colorF)
+        {
+            Bitmap newBmp = new Bitmap(bmp.Width, bmp.Height);
+
+            unsafe
+            {
+                BitmapData bitmapData = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height), ImageLockMode.ReadOnly, PixelFormat.Format32bppRgb);
+                BitmapData newbitmapData = newBmp.LockBits(new Rectangle(0, 0, newBmp.Width, newBmp.Height), ImageLockMode.ReadWrite, PixelFormat.Format32bppRgb);
+
+                byte* floodPointer = (byte*)bitmapData.Scan0;
+                byte* newfloodPointer = (byte*)newbitmapData.Scan0;
+
+                int temp = 0;
+
+                for (int i = 0; i < bmp.Height; i++)
+                {
+                    for (int j = 0; j < bmp.Width; j++)
+                    {
+                        var color = getPixel(bmp, bitmapData, j, i);
+
+                        if (color.B >= idxColor.B - 85 && color.B <= idxColor.B + 85)
+                        {
+                            temp = colorT.B;
+                        }
+                        else
+                        {
+                            temp = colorF.B;
+                        }
+
+                        newfloodPointer[0] = (byte)temp;
+                        newfloodPointer[1] = (byte)temp;
+                        newfloodPointer[2] = (byte)temp;
+
+                        floodPointer += 4;
+                        newfloodPointer += 4;
+                    }
+
+                    floodPointer += bitmapData.Stride - (bitmapData.Width * 4);
+                    newfloodPointer += newbitmapData.Stride - (newbitmapData.Width * 4);
+                }
+                bmp.UnlockBits(bitmapData);
+                newBmp.UnlockBits(newbitmapData);
+            }
+
+            return newBmp;
+        }
+
+        private unsafe Color getPixel(Bitmap bmp, BitmapData bmData, int x, int y)
+        {
+
+            int pixWidth = 4;
+
+            IntPtr scan0 = bmData.Scan0;
+            int stride = bmData.Stride;
+            byte* p = (byte*)scan0.ToPointer() + y * stride;
+            int px = x * pixWidth;
+            Color color = Color.FromArgb(255, p[px + 2], p[px + 1], p[px + 0]);
+
+            return color;
+        }
+
+
+        public Image<Gray, Byte> SkewNiv(Image<Gray, Byte> img)
+        {
+            var unProsImg = img.Copy();
+
+            var radian = Math.Atan2((anglePoint2.Y - anglePoint1.Y), (anglePoint2.X - anglePoint1.X));
+            var angle = radian * (180 / Math.PI);
+
+            return unProsImg.Rotate(Math.Abs(angle), new Gray());
+        }
+
+        public void Dispose()
+        {
+            if (Disposed) return;
+            Disposed = true;
+        }
+    }
+}
